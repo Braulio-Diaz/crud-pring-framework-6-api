27efbcc7d35bad576ae386191437416eb2e52df0
Braulio Díaz <braulio.diaz.gajardo@ciisa.cl>
2024-03-27 22:03:45 -0300

    Se agregaron validaciones para que no se inserten datos nulos en el body al momento de hacer un post o un put

--- src/main/java/com/braulio/curso/springboot/app/springbootcrud/controllers/ProductController.java
+++ src/main/java/com/braulio/curso/springboot/app/springbootcrud/controllers/ProductController.java
@@ -18,6 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.braulio.curso.springboot.app.springbootcrud.entities.Product;
 import com.braulio.curso.springboot.app.springbootcrud.services.ProductService;
 
+import jakarta.validation.Valid;
+
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
@@ -42,13 +44,13 @@ public class ProductController {
     }
 
     @PostMapping("/save")
-    public ResponseEntity<Product> save(@RequestBody Product product){
+    public ResponseEntity<Product> save(@Valid @RequestBody Product product){
         Product newProduct = service.save(product);
         return ResponseEntity.status(HttpStatus.CREATED).body(newProduct);
     }
 
     @PutMapping("/update/{id}")
-    public ResponseEntity<Product> update(@PathVariable Long id,@RequestBody  Product product){
+    public ResponseEntity<Product> update(@PathVariable Long id, @Valid @RequestBody  Product product){
 
         Optional<Product> optionalProduct = service.update(id, product);
         if (optionalProduct.isPresent()) {
